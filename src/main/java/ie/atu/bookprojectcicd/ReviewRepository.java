e3141dc6d5c4054680cfc68ab4f27095fd3016c5
Igorlozko <g00390368@atu.ie>
2023-11-20 22:57:06 +0000

    added the reviews

--- src/main/java/ie/atu/bookprojectcicd/ReviewRepository.java
+++ src/main/java/ie/atu/bookprojectcicd/ReviewRepository.java
@@ -4,11 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface BookRepository extends MongoRepository<Book, ObjectId> {
-    Optional<Book> findBookByAuthor(String author );
-
-    Optional<Object> findById(String id);
+public interface ReviewRepository extends MongoRepository<Book, ObjectId> {
+    Review insert(Review review);
 }
