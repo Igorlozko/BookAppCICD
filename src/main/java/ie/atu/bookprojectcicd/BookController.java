4ee69cda2b49c933e2497b649d48c32c9918adc0
Igorlozko <g00390368@atu.ie>
2023-11-20 22:57:06 +0000

    added the reviews

--- src/main/java/ie/atu/bookprojectcicd/BookController.java
+++ src/main/java/ie/atu/bookprojectcicd/BookController.java
@@ -1,19 +1,29 @@
 package ie.atu.bookprojectcicd;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/api/v1/books")
+@RequestMapping("/api/books")
 public class BookController {
+    @Autowired
+    private BookService bookService;
     @GetMapping
-    public ResponseEntity<String> allBooks(){
-        return new ResponseEntity<String>("All Books", HttpStatus.OK); // return the message all books but with a status of 200 helps with front end
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return new ResponseEntity<List<Book>>(bookService.allBooks(),HttpStatus.OK);
     }
-   /* public String allBooks(){
-        return "All Books";
-    }*/
+
+    @GetMapping("/{author}")
+    public ResponseEntity<Optional<Book>> getsingleBook(@PathVariable String author){
+        return new ResponseEntity<Optional<Book>>(bookService.singleBook(author),HttpStatus.OK);
+    }
 }
---
+++ src/main/java/ie/atu/bookprojectcicd/ReviewService.java
@@ -0,0 +1,23 @@
+package ie.atu.bookprojectcicd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String title){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Book.class).matching(Criteria.where("id").is(title)).apply(new Update().push("reviewIds").value(review)).first();
+
+        return review;
+    }
+}
